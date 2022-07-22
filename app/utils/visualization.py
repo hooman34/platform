40fae55087a5981cf898e03c8cb1727f79c6d623
hooman34 <gieun1505@gmail.com>
2022-07-22 23:38:32 +0900

    fix plot function

--- app/utils/visualization.py
+++ app/utils/visualization.py
@@ -4,45 +4,41 @@ from .log import get_logger
 
 logger = get_logger(__name__)
 
-def _plot_data(plot_type, plot_settings):
+
+def _plot_data(plot_settings):
     """
     Create a plotly figure. The input should be a list of list.
     The inner list should be [dataset, x_column, y_column]
 
     Args:
-        plot_type1(str): type of graph. line or bar
-        plot_settings(list): [[dataset, x_column, y_column], [dataset, x_column, y_column], ...]
+        plot_settings(list): [[plot_type1, dataset, x_column, y_column, data_label], [plot_type1, dataset, x_column, y_column, data_label], ...]
 
     Returns:
         fig(plotly.graph_objs._figure.Figure): plot
     """
-    fig = go.Figure()
 
-    if plot_type=='line':
-        for i, setting in enumerate(plot_settings):
-            trace = _create_trace(plot_type, setting[0], setting[1], setting[2], setting[3], 'y'+str(i+1))
-            fig.add_trace(trace)
-    elif plot_type=='bar':
-        logger.info("bar graph not implemented")
-        return None
-    else:
-        logger.info("specified graph type is not implemented")
-        return None
-    
-    # update fig to make it 
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    for i, setting in enumerate(plot_settings):
+        plot_type = setting[0]
+        assert plot_type in ['scatter', 'line', 'bar']
+        trace = _create_trace(plot_type, setting[1], setting[2], setting[3], setting[4], 'y' + str(i + 1))
+        fig.add_trace(trace, secondary_y=setting[5])
+
+    # update fig to make it
     update_dict = {}
 
-    for i in range(len(plot_settings)-1):
-        update_dict['yaxis'+str(i+2)] = {"overlaying":'y', 'visible': False}
+    for i in range(len(plot_settings) - 1):
+        update_dict['yaxis' + str(i + 2)] = {"overlaying": 'y', 'visible': False}
     fig['layout'].update(update_dict)
-    fig['layout'].update({'yaxis':{'visible':False}})
-    
+    fig['layout'].update({'yaxis': {'visible': False}})
+
     fig.update_layout(legend=dict(
-                        orientation="h",
-                        yanchor="bottom",
-                        y=1.02,
-                        xanchor="right",
-                        x=1))
+        orientation="h",
+        yanchor="bottom",
+        y=1.02,
+        xanchor="right",
+        x=1))
 
     return fig
 
@@ -61,6 +57,13 @@ def _create_trace(plot_type, data, x, y, name, yaxis):
                            y=data[y],
                            name=name,
                            yaxis=yaxis)
+    if plot_type == 'scatter':
+        trace = go.Scatter(x=data[x],
+                           y=data[y],
+                           name=name,
+                           yaxis=yaxis,
+                           mode='markers')
+
     return trace
 
 
